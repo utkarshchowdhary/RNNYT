8a65e62720e70cbc7721b2b40906d8629c30a286
utkarshchowdhary <utkarshchowdhary4140@gmail.com>
2021-06-18 12:57:07 +0530

    removed unused vars

--- src/styles/global.js
+++ src/styles/global.js
@@ -4,12 +4,10 @@ export const DIM_PINK = "#ef798a";
 export const BG_COLOR = "#a3b18a";
 export const BG_COLOR_LIGHT = "#fef9ef";
 export const SHADOW_COLOR = "#a3a380";
-export const BAR_COLOR = "#4e4d52";
 export const TEXT_COLOR = "#ffffff";
 export const BUTTON_COLOR = "#c3baba";
 export const HEADER_TEXT_COLOR = "#ffffff";
 export const MUTED_COLOR = "#646e78";
-export const LINK_COLOR = "#48e9d9";
 export const ACCENT_COLORS = [
   "#a4c3b2",
   "#9cafb7",
